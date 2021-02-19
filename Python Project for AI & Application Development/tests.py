650b3c88718a061e32550fe26d02404799577218
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2021-02-19 20:11:41 +0800

    Add files via upload

--- Python Project for AI & Application Development/tests.py
+++ Python Project for AI & Application Development/tests.py
@@ -4,9 +4,10 @@ from translator import englishtofrench, englishtogerman
 
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(englishtofrench("hello"),"bonjour")
-        self.assertEqual(englishtofrench("welcome"),"bienvenue")
+        self.assertEqual(englishtofrench("hello"),"Bonjour")
+        self.assertEqual(englishtofrench("welcome"),"Bienvenue")
         self.assertEqual(englishtofrench("love"),"Amour")
+        #self.assertEqual(englishtofrench(""),"error")
 
 class TestEnglishToGerman(unittest.TestCase):
     def test1(self):
--- Python Project for AI & Application Development/translator.py
+++ Python Project for AI & Application Development/translator.py
@@ -1,4 +1,6 @@
-#pip install --upgrade ibm-watson
+# sudo python3 -m pip install --upgrade --force pip
+# pip3 install --upgrade ibm-watson
+# pip3 install ibm-cloud-sdk-core
 # pylint: disable=unused-import,line-too-long,bare-except,import-error,invalid-name,missing-final-newline
 
 """This is a translator module"""
@@ -10,6 +12,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def englishtofrench(word):
     """This class does english to french translation"""
+
     url_lt='https://api.us-south.language-translator.watson.cloud.ibm.com/instances/dfb4e059-a2b5-470b-9ac4-f4628099cf0a'
     apikey_lt='5W4TiN2vz1utmA3YgN0-m9qv7TgzznYioDwTXPc4g_-7'
     version_lt='2018-05-01'
@@ -18,15 +21,17 @@ def englishtofrench(word):
     language_translator.set_service_url(url_lt)
     lt = language_translator
     translation = lt.translate(text=word, model_id="en-fr").get_result()
-    try:
-        word=""
-    except:
+    
+    if word == " ":
         print("Please enter a word")
+    else:
+        pass
 
     return translation['translations'][0]['translation']
 
 def englishtogerman(word):
     """This class does english to german translation"""
+
     url_lt='https://api.us-south.language-translator.watson.cloud.ibm.com/instances/dfb4e059-a2b5-470b-9ac4-f4628099cf0a'
     apikey_lt='5W4TiN2vz1utmA3YgN0-m9qv7TgzznYioDwTXPc4g_-7'
     version_lt='2018-05-01'
